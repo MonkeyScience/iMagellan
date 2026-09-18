f8dfab0b98c31d730ea7b8c8eb0355160ce45039
Ch1mpZero <MonkeyScience@users.noreply.github.com>
2026-09-18 09:01:40 +0100

    iMagellan PWA: manifest, icon, service worker, DigitalOcean spec

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE="imagellan-v1";
+const PRE=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRE)).then(()=>self.skipWaiting()));
+});
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener("fetch",e=>{
+  const u=new URL(e.request.url);
+  if(u.origin!==location.origin){
+    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
+      const copy=res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      return res;
+    }).catch(()=>caches.match("./index.html")))
+  );
+});
