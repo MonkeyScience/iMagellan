60d886c3e96aa7731c311de2f94e611562b7cd33
Ch1mpZero <MonkeyScience@users.noreply.github.com>
2026-09-19 13:33:05 +0100

    Official CD heights on Map card from SPP/MAL

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="imagellan-d367-tides";
+const CACHE="imagellan-d367-cdmap";
 const PRE=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRE)).then(()=>self.skipWaiting()));
