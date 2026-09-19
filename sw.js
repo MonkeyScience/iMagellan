facd1e70280a90b3eda77a53a13dfbe26c791f40
Ch1mpZero <MonkeyScience@users.noreply.github.com>
2026-09-19 13:41:33 +0100

    Restore working loader after over-escape; map first

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="imagellan-d367-cdmap";
+const CACHE="imagellan-d367-restore";
 const PRE=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRE)).then(()=>self.skipWaiting()));
